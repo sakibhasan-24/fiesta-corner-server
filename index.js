60e4d768a19bf07d58caa7e82607152978399949
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-12-03 12:07:49 +0600

    add post request on food items add

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -5,5 +7,32 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@food.wlfdec9.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function serverConnection() {
+  const foodItemsCollections = client.db("food-items").collection("food-list");
+  try {
+    await client.connect();
+    // create Food
+    app.post("/food-items", async (req, res) => {
+      const foodData = req.body;
+      const result = await foodItemsCollections.insertOne(foodData);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+serverConnection().catch((err) => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//
